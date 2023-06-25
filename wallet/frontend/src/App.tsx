c439ad61118c97970c71dddd8138c24aef17c206
Elena Fuentes Bongenaar <e.fuentesbongenaar@fastmail.com>
2023-06-24 23:00:36 -0400

    Deploys testcontract & shows balance.

--- wallet/frontend/src/App.tsx
+++ wallet/frontend/src/App.tsx
@@ -13,9 +13,10 @@ function App(this: any) {
   const [pubkey, setPubkey] = useState<any[]>();
   const [show, setShow] = useState(false);
   const [receiver, setReceiver] = useState("");
-  const [Provider, setProvider] = useState<ethers.BrowserProvider | null>(new ethers.BrowserProvider(window.ethereum));
-  const [Signer, setSigner] = useState<ethers.JsonRpcSigner | null>(null);
-  const [ContractAddress, setContractAddress] = useState<string>("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512");
+  const [provider, setProvider] = useState<ethers.BrowserProvider | null>(new ethers.BrowserProvider(window.ethereum));
+  const [signer, setSigner] = useState<ethers.JsonRpcSigner | null>(null);
+  const [contractAddress, setContractAddress] = useState<string | null>("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512");
+  const [balance, setBalance] = useState("");
 
   const handleClosePopup = () => setShow(false);
   const handleShowPopup = () => setShow(true);
@@ -46,7 +47,18 @@ function App(this: any) {
       .catch((err) => {
         console.error("Error checking live status of server:", err);
       });
+
+    getBalances();
   }, [])
+
+  async function getBalances() {
+    if(provider == null || contractAddress == null) {
+      return;
+    }
+    const balance = await provider.getBalance(contractAddress);
+    console.log(balance);
+    setBalance(balance.toString());
+  }
 
   // Create Burner Wallet, by registering on WebAuthn
   async function btnRegBegin() {
@@ -87,30 +99,35 @@ function App(this: any) {
           }
           console.log(pubkeyArray);
 
-          if (Provider === null) {
+          if (provider === null) {
             console.log('Provider not found, this is needed to instantiate Burner wallet...');
             return;
           }
       
-          const signer = Provider.getSigner();
+          const signer = provider.getSigner();
           signer.then((s) => {
             setSigner(s);
-            console.log('signer',s);
 
+            // TODO: this is ERC20 bytecode now so this must be replaced with bytecode for wallet contract.
+            const bytecode = "0x608060405234801561001057600080fd5b506040516103bc3803806103bc83398101604081905261002f9161007c565b60405181815233906000907fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef9060200160405180910390a333600090815260208190526040902055610094565b60006020828403121561008d578081fd5b5051919050565b610319806100a36000396000f3fe608060405234801561001057600080fd5b506004361061004c5760003560e01c8063313ce5671461005157806370a082311461006557806395d89b411461009c578063a9059cbb146100c5575b600080fd5b604051601281526020015b60405180910390f35b61008e610073366004610201565b6001600160a01b031660009081526020819052604090205490565b60405190815260200161005c565b604080518082018252600781526626bcaa37b5b2b760c91b6020820152905161005c919061024b565b6100d86100d3366004610222565b6100e8565b604051901515815260200161005c565b3360009081526020819052604081205482111561014b5760405162461bcd60e51b815260206004820152601a60248201527f696e73756666696369656e7420746f6b656e2062616c616e6365000000000000604482015260640160405180910390fd5b336000908152602081905260408120805484929061016a9084906102b6565b90915550506001600160a01b0383166000908152602081905260408120805484929061019790849061029e565b90915550506040518281526001600160a01b0384169033907fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef9060200160405180910390a350600192915050565b80356001600160a01b03811681146101fc57600080fd5b919050565b600060208284031215610212578081fd5b61021b826101e5565b9392505050565b60008060408385031215610234578081fd5b61023d836101e5565b946020939093013593505050565b6000602080835283518082850152825b818110156102775785810183015185820160400152820161025b565b818111156102885783604083870101525b50601f01601f1916929092016040019392505050565b600082198211156102b1576102b16102cd565b500190565b6000828210156102c8576102c86102cd565b500390565b634e487b7160e01b600052601160045260246000fdfea2646970667358221220d80384ce584e101c5b92e4ee9b7871262285070dbcd2d71f99601f0f4fcecd2364736f6c63430008040033";
             const abi = [
-              "function fibonacci(uint256 n) external view returns (uint256)",
-              "function calculateFibonacci(uint256 n) external",
+              "constructor(uint totalSupply)"
             ];
-            const contract = new ethers.Contract(ContractAddress, abi, s);
-        
-            contract.calculateFibonacci(5)
-              .then((t) => {
-                console.log(t);
-                setPubkey(pubkeyArray);
-                // TODO call contract and get burner wallet address. Set address. 
-              }).catch(() => {
-                console.log("transaction rejected");
-              })
+            const factory = new ethers.ContractFactory(abi, bytecode, s);
+            factory.deploy(ethers.parseUnits("100")).then((contract) => {
+              console.log("deploy started...");
+              return contract.waitForDeployment();
+            }).then((result) => {
+              console.log("deploy completed. Getting address...");
+              return result.getAddress();
+            }).then((address) => {
+              console.log(address);
+              setPubkey(pubkeyArray);
+              setContractAddress(address);
+              return getBalances();
+            }).then(() => {
+              console.log("Updated balances.");
+            });
           });
 
         } else {
@@ -166,18 +183,6 @@ function App(this: any) {
       <div style={wallet_page} className="m-0 p-0 container-xxl text-center" >
 
         <div className="row justify-content-center pt-5">
-          <div className="col-6">
-            <div className="card p-3" style={balance_card}>
-              <div className="card-header text-start">
-                <h2>Balance</h2>
-              </div>
-
-              <div className="card-body">
-                {/* TODO add balances */}
-              </div>
-            </div>
-          </div>
-
           <div className="col-4">
             <div className="card p-3" style={wallet_card}>
             <div className="card-header">
@@ -188,11 +193,13 @@ function App(this: any) {
               <QRCode
                 size={256}
                 style={{ height: "auto", maxWidth: "100%", width: "100%"}}
-                value={pubkey.toString()} //TODO this needs to be the address
+                value={contractAddress || "uninitialized"} //TODO this needs to be the address
                 viewBox={`0 0 256 256`}
                 /><br></br>
-                <p className="pt-3">0xb794f5ea0ba39494ce839613fffba74279579268</p> {/*TODO this needs to be the address*/}
+                <p className="pt-3">{contractAddress}</p>
               </div>
+
+              <p>Current balance: {balance}</p>
 
               <Button style={buttonCreateStyle}  onClick={handleShowPopup}>
               Send funds
